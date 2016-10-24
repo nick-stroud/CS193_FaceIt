77f98b6010d15a45e626e8ab57d6a726e12d6ffd
ov141d <nicholas.stroud@gmail.com>
2016-10-23 18:43:24 -0700

    Initial Commit

---
+++ CS193_FaceIt/FaceViewController.swift
@@ -0,0 +1,15 @@
+//
+//  ViewController.swift
+//  CS193_FaceIt
+//
+//  Created by Nicholas Stroud on 10/23/16.
+//  Copyright © 2016 Nicholas Stroud. All rights reserved.
+//
+
+import UIKit
+
+class FaceViewController: UIViewController {
+
+
+}
+
